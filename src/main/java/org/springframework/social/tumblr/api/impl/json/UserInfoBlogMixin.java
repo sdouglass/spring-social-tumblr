0d3758bf504f66f482ebcce3867294f3c3daea33
Constantin Miroslav <costin.miroslav@gmail.com>
2012-03-29 11:59:02 +0300

    unrecognized field "facebook_opengraph_enabled" UserInfoBlog

--- src/main/java/org/springframework/social/tumblr/api/impl/json/UserInfoBlogMixin.java
+++ src/main/java/org/springframework/social/tumblr/api/impl/json/UserInfoBlogMixin.java
@@ -1,11 +1,18 @@
 package org.springframework.social.tumblr.api.impl.json;
 
 import org.codehaus.jackson.annotate.JsonProperty;
+import org.codehaus.jackson.map.annotate.JsonDeserialize;
 
 @SuppressWarnings("unused")
 public class UserInfoBlogMixin {
 	
   @JsonProperty("ask_anon")
   private boolean askAnon;
+  
+  @JsonProperty("facebook_opengraph_enabled")
+  @JsonDeserialize(using = BooleanYNDeserializer.class)
+  private boolean facebookOpengraphEnabled;
+
+
 
 }
